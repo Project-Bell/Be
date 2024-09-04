cfdd55deb73ccfa93957bd7ac135fea4bec34ed4
hyunjun-11 <mical0108@gmail.com>
2024-09-04 18:40:26 +0900

    firstCommit

---
+++ src/main/java/org/bell/BellApplication.java
@@ -0,0 +1,13 @@
+package org.bell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BellApplication.class, args);
+    }
+
+}

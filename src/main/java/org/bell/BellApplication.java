dcfa9ccd907cd132e4e45f4fd99f7adc33843d40
hyunjun-11 <mical0108@gmail.com>
2024-09-15 19:03:55 +0900

    feat : @EnableJpaAuditing

--- src/main/java/org/bell/BellApplication.java
+++ src/main/java/org/bell/BellApplication.java
@@ -1,14 +1,26 @@
 package org.bell;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication( exclude = SecurityAutoConfiguration.class )
+@EnableJpaAuditing
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class BellApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BellApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }

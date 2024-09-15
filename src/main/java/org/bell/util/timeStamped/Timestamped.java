1af66dada0018d2f0c3679356319162f70530d65
hyunjun-11 <mical0108@gmail.com>
2024-09-15 19:03:09 +0900

    feat : Timestamped

---
+++ src/main/java/org/bell/util/timeStamped/Timestamped.java
@@ -0,0 +1,28 @@
+package org.bell.util.timeStamped;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Timestamped {
+    //테스트
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
